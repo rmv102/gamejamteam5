94bbac896e1449eede442d57df35ee9fedf2c831
boltonryan <137221862+boltonryan@users.noreply.github.com>
2025-09-27 16:23:59 -0400

    Added UI and GameManage Logic

---
+++ Assets/Scripts/ScoreTracker.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreTracker : MonoBehaviour
+{
+    [SerializeField] Image progressBar;      // Image Type = Filled
+    [SerializeField] Text feedbackText;
+    public string particleTag = "InfectedParticle";
+    public int maxParticles = 100; // your cap
+
+    void Update()
+    {
+        int current = GameObject.FindGameObjectsWithTag(particleTag).Length;
+        float percentGenerated = Mathf.Clamp01((float)current / Mathf.Max(1, maxParticles));
+        progressBar.fillAmount = percentGenerated;
+
+        int percent = Mathf.RoundToInt(percentGenerated * 100f);
+
+        if (percent <= 20)
+            feedbackText.text = "Healthy";
+        else if (percent <= 40)
+            feedbackText.text = "Minor Infection";
+        else if (percent <= 60)
+            feedbackText.text = "Spreading Infection";
+        else if (percent <= 80)
+            feedbackText.text = "Severe Infection";
+        else
+            feedbackText.text = "Fully Infected!";
+    }
+
+}
